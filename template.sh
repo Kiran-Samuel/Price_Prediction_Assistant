427ddb72f98cd7061db8aecdd53da57fbb299575
Kiran-Samuel <kirankoshy41@gmail.com>
2026-01-31 14:31:08 +0400

    Folder structure and files created

---
+++ template.sh
@@ -0,0 +1,26 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+mkdir -p data
+mkdir -p model
+# Creating files
+touch data
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch src/data_prep.py
+touch src/feature_engineering.py
+touch src/price_regression.py
+touch src/win_rate_analysis.py
+touch src/business_rules.py
+touch src/llm_reasoning.py
+touch .env
+touch setup.py
+touch app.py
+touch model
+touch research/trials.ipynb
+touch research/eda.ipynb
+touch research/feature_tests.ipynb
+touch requirements.txt
+
+echo "Directories and files created successfully"
